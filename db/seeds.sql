adda6ae611de0c86d5c70daef748e4e094f826a7
Luzanne Burt <luzannea@gmail.com>
2019-07-10 10:08:37 -0400

    functioning properly

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name) VALUES ("Bleu Cheese Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Bacon Cheese Burger");
+
+INSERT INTO burgers (burger_name) VALUES ("Triple Cheese Burger");
